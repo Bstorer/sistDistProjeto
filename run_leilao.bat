8cdf52acfba60e5ebfc58cf8636b02c8aa6e409c
Felipe Cardoso <felipehasc@gmail.com>
2021-07-29 20:46:54 -0300

    Removed old files and javadocs

--- run_leilao.bat
+++
@@ -1,6 +0,0 @@
-set ZK="C:\opt\zookeeper"
-set CP_ZK=.;%ZK%\zookeeper-3.4.14.jar;%ZK%\lib\slf4j-log4j12-1.7.25.jar;%ZK%\lib\slf4j-api-1.7.25.jar;%ZK%\lib\log4j-1.2.17.jar
-javac -cp %CP_ZK% *.java
-@echo ***** Leilao test
-java -cp %CP_ZK% -Dlog4j.configuration=file:%ZK%\conf\log4j.properties SyncPrimitive localhost 
-
